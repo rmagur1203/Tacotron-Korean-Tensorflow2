bd555c98af0bbb890684f910c0f847b6bba76778
rmagur1203 <rmagur1203@naver.com>
2023-06-08 10:48:12 +0900

    train2 tpu

--- train2.py
+++ train2.py
@@ -11,6 +11,13 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from models.tacotron import post_CBHG
 from util.hparams import *
 
+resolver = tf.distribute.cluster_resolver.TPUClusterResolver(
+    tpu='grpc://' + os.environ['COLAB_TPU_ADDR'])
+
+tf.config.experimental_connect_to_cluster(resolver)
+tf.tpu.experimental.initialize_tpu_system(resolver)
+
+strategy = tf.distribute.TPUStrategy(resolver)
 
 data_dir = './data'
 mel_list = sorted(glob.glob(os.path.join(data_dir + '/mel', '*.npy')))
@@ -61,34 +68,31 @@ def train_step(mel_input, spec_target):
     return loss, pred[0]
 
 
-dataset = tf.data.Dataset.from_generator(generator=DataGenerator,
-                                         output_types=(tf.float32, tf.float32),
-                                         output_shapes=(tf.TensorShape([batch_size, None, mel_dim]),
-                                                        tf.TensorShape([batch_size, None, n_fft // 2 + 1])))\
-    .prefetch(tf.data.experimental.AUTOTUNE)
-
-model = post_CBHG(K=8, conv_dim=[256, mel_dim])
+with strategy.scope():
+    model = post_CBHG(K=8, conv_dim=[256, mel_dim])
 optimizer = Adam()
 step = tf.Variable(0)
 
+local_device_option = tf.train.CheckpointOptions(
+    experimental_io_device="/job:localhost")
 checkpoint_dir = './checkpoint/2'
 os.makedirs(checkpoint_dir, exist_ok=True)
 checkpoint = tf.train.Checkpoint(optimizer=optimizer, model=model, step=step)
 manager = tf.train.CheckpointManager(checkpoint, checkpoint_dir, max_to_keep=5)
 
-checkpoint.restore(manager.latest_checkpoint)
+checkpoint.restore(manager.latest_checkpoint, local_device_option)
 if manager.latest_checkpoint:
     print('Restore checkpoint from {}'.format(manager.latest_checkpoint))
 
 try:
-    for mel, spec in dataset:
+    for mel, spec in DataGenerator():
         loss, pred = train_step(mel, spec)
         checkpoint.step.assign_add(1)
         print("Step: {}, Loss: {:.5f}".format(int(checkpoint.step), loss))
 
         if int(checkpoint.step) % checkpoint_step == 0:
             checkpoint.save(file_prefix=os.path.join(
-                checkpoint_dir, 'step-{}'.format(int(checkpoint.step))))
+                checkpoint_dir, 'step-{}'.format(int(checkpoint.step))), options=local_device_option)
 
 except Exception:
     traceback.print_exc()
